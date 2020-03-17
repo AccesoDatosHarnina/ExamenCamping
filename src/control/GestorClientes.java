874d8ba8e6456d174f20b22e945632c366a85878
programacion <>
2020-03-17 09:33:12 +0100

    dia dos

--- src/control/GestorClientes.java
+++ src/control/GestorClientes.java
@@ -3,23 +3,20 @@ package control;
 import modelo.Cliente;
 
 public class GestorClientes {
-	int cantidad=5;
+	int cantidad = 5;
 	public Cliente[] clientes;
 
 	public GestorClientes() {
 		super();
-		clientes=GeneradorClientes.generaClientes(cantidad);
+		clientes = GeneradorClientes.generaClientes(cantidad);
 	}
 
 	public Cliente getCliente(Integer id) {
-		Cliente retorno=null;
-		if(id<clientes.length) {
-			retorno=clientes[id];
+		Cliente retorno = null;
+		if (id < clientes.length) {
+			retorno = clientes[id];
 		}
 		return retorno;
 	}
-	
-	
-	
 
 }
